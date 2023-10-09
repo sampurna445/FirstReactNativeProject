207cd5c36ed8f52c52417046ef9787d95fa6165d
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-10-09 16:34:26 +0100

    installed redux-logger

--- store.js
+++ store.js
@@ -1,8 +1,19 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from './features/counter/counterSlice';
+import {createLogger} from 'redux-logger';
+
+const isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent;
+
+const logger = createLogger({
+  preidicate: () => isDebuggingInChrome,
+  collapsed: true,
+  duration: true,
+  diff: true,
+});
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
