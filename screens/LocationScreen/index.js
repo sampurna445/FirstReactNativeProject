4b9ee4e5c58cddf5f00968e6a6bdd845ca885390
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-11-03 14:12:15 +0000

    Added react native maps

--- screens/LocationScreen/index.js
+++ screens/LocationScreen/index.js
@@ -1,13 +1,32 @@
 import React, {useEffect} from 'react';
 import {View, Text, Platform} from 'react-native';
 import {LocationHelper} from '../../helpers';
+import MapView from 'react-native-maps';
 
 const LocationScreen = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    LocationHelper.fetchLocation(
+      locationObject => {
+        console.log(locationObject);
+      },
+      error => {
+        console.log(error);
+      },
+    );
+  }, []);
 
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Text>Testing Location Service</Text>
+      <MapView
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
