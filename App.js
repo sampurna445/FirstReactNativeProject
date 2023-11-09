c48d848d252c645d324f61c5fc23180252b58c9c
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-11-09 16:12:32 +0000

    Crypto helper added

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Navigator from './navigation';
-import {PersistanceHelper} from './helpers';
+import {PersistanceHelper, CryptoHelpers} from './helpers';
 import {store} from './store';
 import {Provider} from 'react-redux';
 
@@ -16,6 +16,11 @@ function App() {
   useEffect(() => {
     // PersistanceHelper.setValue('testkey', 'testvalue');
     PersistanceHelper.setObject('testkey', {a: 'b', c: 'd', e: 'f'});
+
+    const encryptedString = CryptoHelpers.encryptString(
+      'Text for testing Cryptography',
+    );
+    console.log("Encrypted String: " + encryptedString);
   }, []);
   return (
     <Provider store={store}>
--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 import PersistanceHelper from './PersistanceHelper';
 import ApiHelper from './ApiHelper';
 import LocationHelper from './LocationHelper';
+import CryptoHelpers from './CryptoHelpers';
 
-export {PersistanceHelper, ApiHelper, LocationHelper};
+export {PersistanceHelper, ApiHelper, LocationHelper, CryptoHelpers};
